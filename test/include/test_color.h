152ea939ad4633a05db682aad5400b5962311c29
Weeley Chong <cckwes@gmail.com>
2016-01-13 05:53:20 +0800

    completed test functions for color

--- test/include/test_color.h
+++ test/include/test_color.h
@@ -12,4 +12,14 @@ bool test_bgr_to_rgb(qivon::Image<unsigned char> &_src, size_t _run_time = 1);
 
 bool test_gamma_correction(qivon::Image<unsigned char> &_src, float _gamma, size_t _run_time = 1);
 
+bool test_brightness_adjustment(qivon::Image<unsigned char> &_src, int _adjustment, size_t _run_time = 1);
+
+bool test_contrast_adjustment(qivon::Image<unsigned char> &_src, int _adjustment, size_t _run_time = 1);
+
+bool test_white_balance_adjustment(qivon::Image<unsigned char> &_src, int _adjustment, size_t _run_time = 1);
+
+bool test_hue_adjustment(qivon::Image<unsigned char> &_src, int _value, size_t _run_time = 1);
+
+bool test_saturation_adjustment(qivon::Image<unsigned char> &_src, int _value, size_t _run_time = 1);
+
 #endif    //TEST_COLOR_H
--- test/src/test_color.cpp
+++ test/src/test_color.cpp
@@ -74,3 +74,117 @@ bool test_bgr_to_rgb(qivon::Image<unsigned char> &_src, size_t _run_time) {
   else
     return qivon::writePngFile("bgr_to_rgb.png", rgb);
 }
+
+bool test_gamma_correction(qivon::Image<unsigned char> &_src, float _gamma, size_t _run_time) {
+  if (_src.isEmpty())
+    return false;
+
+  qivon::Image<unsigned char> gamma;
+
+  set_start_now();
+
+  for (size_t i = 0; i < _run_time; ++i)
+    qivon::gamma_correction_LUT(_src, gamma, _gamma);
+
+  std::cout << __FUNCTION__ << " time " << get_elapsed_time() / _run_time << " ms\n";
+
+  if (gamma.isEmpty())
+    return false;
+  else
+    return qivon::writePngFile("gamma.png", gamma);
+}
+
+bool test_brightness_adjustment(qivon::Image<unsigned char> &_src, int _adjustment, size_t _run_time) {
+  if (_src.isEmpty())
+    return false;
+
+  qivon::Image<unsigned char> brightness;
+
+  set_start_now();
+
+  for (size_t i = 0; i < _run_time; ++i)
+    qivon::brightness_adjustment(_src, brightness, _adjustment);
+
+  std::cout << __FUNCTION__ << " time " << get_elapsed_time() / _run_time << " ms\n";
+
+  if (brightness.isEmpty())
+    return false;
+  else
+    return qivon::writePngFile("brightness.png", brightness);
+}
+
+bool test_contrast_adjustment(qivon::Image<unsigned char> &_src, int _adjustment, size_t _run_time) {
+  if (_src.isEmpty())
+    return false;
+
+  qivon::Image<unsigned char> contrast;
+
+  set_start_now();
+
+  for (size_t i = 0; i < _run_time; ++i)
+    qivon::contrast_adjustment(_src, contrast, _adjustment);
+
+  std::cout << __FUNCTION__ << " time " << get_elapsed_time() / _run_time << " ms\n";
+
+  if (contrast.isEmpty())
+    return false;
+  else
+    return qivon::writePngFile("contrast.png", contrast);
+}
+
+bool test_white_balance_adjustment(qivon::Image<unsigned char> &_src, int _adjustment, size_t _run_time) {
+  if (_src.isEmpty())
+    return false;
+
+  qivon::Image<unsigned char> wb;
+
+  set_start_now();
+
+  for (size_t i = 0; i < _run_time; ++i)
+    qivon::white_balance_adjustment(_src, wb, _adjustment);
+
+  std::cout << __FUNCTION__ << " time " << get_elapsed_time() / _run_time << " ms\n";
+
+  if (wb.isEmpty())
+    return false;
+  else
+    return qivon::writePngFile("wb.png", wb);
+}
+
+bool test_hue_adjustment(qivon::Image<unsigned char> &_src, int _value, size_t _run_time) {
+  if (_src.isEmpty())
+    return false;
+
+  qivon::Image<unsigned char> hue;
+
+  set_start_now();
+
+  for (size_t i = 0; i < _run_time; ++i)
+    qivon::hue_adjustment(_src, hue, _value);
+
+  std::cout << __FUNCTION__ << " time " << get_elapsed_time() / _run_time << " ms\n";
+
+  if (hue.isEmpty())
+    return false;
+  else
+    return qivon::writePngFile("hue.png", hue);
+}
+
+bool test_saturation_adjustment(qivon::Image<unsigned char> &_src, int _value, size_t _run_time) {
+  if (_src.isEmpty())
+    return false;
+
+  qivon::Image<unsigned char> saturation;
+
+  set_start_now();
+
+  for (size_t i = 0; i < _run_time; ++i)
+    qivon::saturation_adjustment(_src, saturation, _value);
+
+  std::cout << __FUNCTION__ << " time " << get_elapsed_time() / _run_time << " ms\n";
+
+  if (saturation.isEmpty())
+    return false;
+  else
+    return qivon::writePngFile("saturation.png", saturation);
+}
